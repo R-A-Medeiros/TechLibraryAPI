8490ad798d733c0212e250fdde72ea6161af0f69
Rinaldo Alves <r.medeiros1206@gmail.com>
2025-06-27 09:52:13 -0300

    entidade usuario

---
+++ src/core/FCxLabs.TechLibraryAPI.Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace FCxLabs.TechLibraryAPI.Domain.Entities;
+
+public class User
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public Guid UserIdentifier { get; set; }
+    public string Role { get; set; } = string.Empty;
+}
---
+++ src/core/FCxLabs.TechLibraryAPI.Domain/Enums/Roles.cs
@@ -0,0 +1,7 @@
+﻿namespace FCxLabs.TechLibraryAPI.Domain.Enums;
+
+public static class Roles
+{
+    public const string ADMIN = "administrator";
+    public const string TEAM_MEMBER = "teamMember";
+}
