f52adc5dddfa0f158f6cba044e560d9fcc3160f6
Rinaldo Alves <r.medeiros1206@gmail.com>
2025-08-05 01:27:01 -0300

    Concluindo testes de integração para o sucesso

--- tests/WebApi.Test/Users/Register/RegisterUserTest.cs
+++ tests/WebApi.Test/Users/Register/RegisterUserTest.cs
@@ -1,27 +1,41 @@
 ﻿using System.Net.Http.Json;
+using System.Text.Json;
 using CommonTestUtilities.Requests;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace WebApi.Test.Users.Register;
 
-public class RegisterUserTest : IClassFixture<WebApplicationFactory<Program>>
+public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
 {
     private const string METHOD = "api/User";
 
     private readonly HttpClient _httpClient;
-    public RegisterUserTest(WebApplicationFactory<Program> webApplicationFactory)
+    public RegisterUserTest(CustomWebApplicationFactory webApplicationFactory)
     {
         _httpClient = webApplicationFactory.CreateClient();
     }
 
     [Fact]
     public async Task Success()
-    {
+     {
         var request = RequestRegisterUserJsonBuilder.Build();
 
         var result = await _httpClient.PostAsJsonAsync(METHOD, request);
 
-        result.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+        //result.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+        var body = await result.Content.ReadAsStreamAsync();
+
+        var response = await JsonDocument.ParseAsync(body);
+
+        response.RootElement
+            .GetProperty("name")
+            .GetString()
+            .Should().Be(request.Name);
+
+        response.RootElement
+            .GetProperty("token")
+            .GetString()
+            .Should().NotBeNullOrEmpty();
     }
 }
