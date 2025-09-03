fa683f789805eeb71665404d5c911139acb3c848
Rinaldo Alves <r.medeiros1206@gmail.com>
2025-09-03 02:30:07 -0300

    Atualização do DbContext com as novas entidades

--- src/infrastructure/FCxLabs.TechLibraryAPI.Infrastructure/DataAccess/TechLibraryDbContext.cs
+++ src/infrastructure/FCxLabs.TechLibraryAPI.Infrastructure/DataAccess/TechLibraryDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using FCxLabs.TechLibraryAPI.Domain.Entities;
+using FCxLabs.TechLibraryAPI.Domain.Enums;
 using Microsoft.EntityFrameworkCore;
 
 namespace FCxLabs.TechLibraryAPI.Infrastructure.DataAccess;
@@ -10,11 +11,35 @@ public class TechLibraryDbContext : DbContext
     public DbSet<Author> Authors { get; set; }
     public DbSet<User> Users { get; set; }
     public DbSet<LogAction> Logs { get; set; }
+    public DbSet<BookCopy> BookCopies { get; set; }
+    public DbSet<Loan> Loans { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Book>()
                     .HasOne(b => b.Author)
                     .WithMany(a => a.Books)
                     .HasForeignKey(b => b.AuthorId);
+
+        
+        modelBuilder.Entity<Book>()
+            .HasMany(b => b.Copies)
+            .WithOne(c => c.Book)
+            .HasForeignKey(c => c.BookId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        
+        modelBuilder.Entity<BookCopy>()
+            .HasMany<Loan>()
+            .WithOne(l => l.BookCopy)
+            .HasForeignKey(l => l.BookCopyId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        
+        modelBuilder.Entity<User>()
+            .HasMany<Loan>()
+            .WithOne(l => l.User)
+            .HasForeignKey(l => l.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
